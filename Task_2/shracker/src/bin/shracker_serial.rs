c66ee82dd8820cb3be2be208da14b76922c844b3
Radonoxius <Radonoxius@protonmail.com>
2025-10-18 00:57:04 +0530

    Added sequential version of shracker. (Untested)

--- Task_2/shracker/src/bin/shracker_serial.rs
+++ Task_2/shracker/src/bin/shracker_serial.rs
@@ -1,3 +1,35 @@
+use std::time::Instant;
+
+use shracker::{generate_sha256_hash, HASH1};
+
 fn main() {
-    println!("Hello, world!");
+    let start_time = Instant::now();
+
+    'outer: for c1 in 'a'..='z' {
+        for c2 in 'a'..='z' {
+            for c3 in 'a'..='z' {
+                for c4 in 'a'..='z' {
+                    for c5 in 'a'..='z' {
+                        for c6 in 'a'..='z' {
+                            let mut word = String::with_capacity(6);
+                            word.push(c1);
+                            word.push(c2);
+                            word.push(c3);
+                            word.push(c4);
+                            word.push(c5);
+                            word.push(c6);
+
+                            if HASH1 == generate_sha256_hash(&word) {
+                                println!("The word is: {}", word);
+                                break 'outer;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let duration = start_time.elapsed();
+    println!("The computation took: {} s", duration.as_secs());
 }
