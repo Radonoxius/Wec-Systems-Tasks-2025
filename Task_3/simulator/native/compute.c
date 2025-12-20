8f30a21cab36b3dd2cc874683db8993afe8eea35
Radonoxius <Radonoxius@protonmail.com>
2025-12-20 22:41:50 +0530

    Seperated native source into a header. Also added OpenCL version checking.

--- Task_3/simulator/native/compute.c
+++ Task_3/simulator/native/compute.c
@@ -1,149 +1,4 @@
-#define _POSIX_C_SOURCE 200809L
-
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <time.h>
-
-#include <CL/cl.h>
-
-//Device could be GPU, CPU, NPU, anything!
-//Holds temporary values to be sent to Device...
-uint8_t* TEMP;
-
-//A T-Flip-Flop initialized to 0?
-uint8_t TFF = 0;
-
-//Resets done to 0
-void reset_done(cl_command_queue queue, uint32_t* done) {
-    clEnqueueSVMMap(
-        queue,
-        CL_TRUE,
-        CL_MAP_WRITE,
-        done,
-        sizeof(uint32_t),
-        0,
-        nullptr,
-        nullptr
-    );
-
-    *done = 0;
-
-    clEnqueueSVMUnmap(queue, done, 0, nullptr, nullptr);
-}
-
-//Gives `true` if done = 0, basically the simulation ends
-bool is_done(cl_command_queue queue, uint32_t* done) {
-    bool result = false;
-
-    clEnqueueSVMMap(
-        queue,
-        CL_TRUE,
-        CL_MAP_READ,
-        done,
-        sizeof(uint32_t),
-        0,
-        nullptr,
-        nullptr
-    );
-
-    if (*done == 0) {
-        result = true;
-    }
-    
-    clEnqueueSVMUnmap(queue, done, 0, nullptr, nullptr);
-    return result;
-}
-
-//Gets the next `logical` tree grid
-uint8_t* get_next_tree_grid(uint8_t* a, uint8_t* b) {
-    if (TFF)
-        return a;
-    else
-        return b;
-}
-
-//Gets the current `logical` tree grid
-uint8_t* get_tree_grid(uint8_t* a, uint8_t* b) {
-    if (TFF)
-        return b;
-    else
-        return a;
-}
-
-//Gets random bytes on linux such that, p(1) = 0.25 & p(0) = 0.75
-void get_randoms(
-    cl_command_queue queue,
-    uint8_t *buffer,
-    const size_t len
-) {
-    FILE *fp = fopen("/dev/random", "rb");
-    fread(TEMP, sizeof(uint8_t), len, fp);
-    fclose(fp);
-
-    for (size_t i = 0; i < len; i++)
-        TEMP[i] = (TEMP[i] < 64) ? 1 : 0;
-
-    clEnqueueSVMMemcpy(
-        queue,
-        CL_FALSE,
-        buffer,
-        TEMP,
-        len,
-        0,
-        nullptr,
-        nullptr
-    );
-}
-
-//Initialises the tree grid to be simulated
-void initialize(
-    cl_command_queue queue,
-    uint8_t *buffer,
-    const size_t len,
-    const uint32_t tree_grid_size,
-    const uint8_t factor,
-    const bool print_enabled
-) {
-    FILE *fp = fopen("/dev/random", "rb");
-    fread(TEMP, sizeof(uint8_t), len, fp);
-    fclose(fp);
-
-    for (size_t i = 0; i < len; i++)
-        TEMP[i] = (TEMP[i] <= factor) ? 1 : 0;
-
-    for (size_t i = 0; i < tree_grid_size + 2; i++) {
-        TEMP[i] = 3;
-        TEMP[(tree_grid_size + 2) * (tree_grid_size + 1) + i] = 3;
-    }
-
-    for (size_t i = 1; i <= tree_grid_size; i++) {
-        TEMP[i * (tree_grid_size + 2)] = 3;
-        TEMP[i * (tree_grid_size + 2) + tree_grid_size + 1] = 3;
-    }
-
-    if (print_enabled) {
-        printf("At start:\n");
-        for (size_t j = 0; j < tree_grid_size; j++) {
-            for (size_t i = 0; i < tree_grid_size; i++) {
-                printf("%d", TEMP[(j + 1) * (tree_grid_size + 2) + i + 1]);
-            }
-            printf("\n");
-        }
-        printf("\n");
-    }
-
-    clEnqueueSVMMemcpy(
-        queue,
-        CL_TRUE,
-        buffer,
-        TEMP,
-        len,
-        0,
-        nullptr,
-        nullptr
-    );
-}
+#include "init.h"
 
 //Simulate it!
 uint64_t simulate(
@@ -154,24 +9,17 @@ uint64_t simulate(
     const bool print_enabled
 ) {
     uint64_t epoch_count = 0;
+    
+    //Device initialisation stuff...
+    cl_device_id device = choose_device();
 
     //Device initialisation stuff...
     cl_platform_id platform;
-
-    clGetPlatformIDs(
-        1,
+    clGetDeviceInfo(
+        device,
+        CL_DEVICE_PLATFORM,
+        sizeof(cl_platform_id),
         &platform,
-        nullptr
-    );
-    
-    //Device initialisation stuff...
-    cl_device_id device;
-
-    clGetDeviceIDs(
-        platform,
-        CL_DEVICE_TYPE_GPU,
-        1,
-        &device,
         nullptr
     );
 
@@ -199,7 +47,7 @@ uint64_t simulate(
         shader,
         1,
         &device,
-        "-cl-mad-enable -cl-std=CL2.0 -Wall -Werror",
+        "-cl-mad-enable -cl-std=CL2.0",
         nullptr,
         nullptr
     );
@@ -252,7 +100,7 @@ uint64_t simulate(
         sizeof(uint8_t) * extended_grid_size,
         0
     );
-    initialize(
+    initialize_trees(
         queue,
         tree_grid,
         extended_grid_size,
---
+++ Task_3/simulator/native/init.h
@@ -0,0 +1,261 @@
+#pragma once
+
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+
+#include <CL/cl.h>
+
+//Device could be GPU, CPU, NPU, anything!
+//Holds temporary values to be sent to Device...
+uint8_t* TEMP;
+
+//A T-Flip-Flop initialized to 0?
+uint8_t TFF = 0;
+
+//Resets done to 0
+void reset_done(cl_command_queue queue, uint32_t* done) {
+    clEnqueueSVMMap(
+        queue,
+        CL_TRUE,
+        CL_MAP_WRITE,
+        done,
+        sizeof(uint32_t),
+        0,
+        nullptr,
+        nullptr
+    );
+
+    *done = 0;
+
+    clEnqueueSVMUnmap(queue, done, 0, nullptr, nullptr);
+}
+
+//Gives `true` if done = 0, basically the simulation ends
+bool is_done(cl_command_queue queue, uint32_t* done) {
+    bool result = false;
+
+    clEnqueueSVMMap(
+        queue,
+        CL_TRUE,
+        CL_MAP_READ,
+        done,
+        sizeof(uint32_t),
+        0,
+        nullptr,
+        nullptr
+    );
+
+    if (*done == 0) {
+        result = true;
+    }
+    
+    clEnqueueSVMUnmap(queue, done, 0, nullptr, nullptr);
+    return result;
+}
+
+//Gets the current `logical` tree grid
+uint8_t* get_tree_grid(uint8_t* a, uint8_t* b) {
+    if (TFF)
+        return b;
+    else
+        return a;
+}
+
+//Gets the next `logical` tree grid
+uint8_t* get_next_tree_grid(uint8_t* a, uint8_t* b) {
+    if (TFF)
+        return a;
+    else
+        return b;
+}
+
+//Gets random bytes on linux such that, p(1) = 0.25 & p(0) = 0.75
+void get_randoms(
+    cl_command_queue queue,
+    uint8_t *buffer,
+    const size_t len
+) {
+    FILE *fp = fopen("/dev/random", "rb");
+    fread(TEMP, sizeof(uint8_t), len, fp);
+    fclose(fp);
+
+    for (size_t i = 0; i < len; i++)
+        TEMP[i] = (TEMP[i] < 64) ? 1 : 0;
+
+    clEnqueueSVMMemcpy(
+        queue,
+        CL_FALSE,
+        buffer,
+        TEMP,
+        len,
+        0,
+        nullptr,
+        nullptr
+    );
+}
+
+//Initialises the tree grid to be simulated
+void initialize_trees(
+    cl_command_queue queue,
+    uint8_t *buffer,
+    const size_t len,
+    const uint32_t tree_grid_size,
+    const uint8_t factor,
+    const bool print_enabled
+) {
+    FILE *fp = fopen("/dev/random", "rb");
+    fread(TEMP, sizeof(uint8_t), len, fp);
+    fclose(fp);
+
+    for (size_t i = 0; i < len; i++)
+        TEMP[i] = (TEMP[i] <= factor) ? 1 : 0;
+
+    for (size_t i = 0; i < tree_grid_size + 2; i++) {
+        TEMP[i] = 3;
+        TEMP[(tree_grid_size + 2) * (tree_grid_size + 1) + i] = 3;
+    }
+
+    for (size_t i = 1; i <= tree_grid_size; i++) {
+        TEMP[i * (tree_grid_size + 2)] = 3;
+        TEMP[i * (tree_grid_size + 2) + tree_grid_size + 1] = 3;
+    }
+
+    if (print_enabled) {
+        printf("At start:\n");
+        for (size_t j = 0; j < tree_grid_size; j++) {
+            for (size_t i = 0; i < tree_grid_size; i++) {
+                printf("%d", TEMP[(j + 1) * (tree_grid_size + 2) + i + 1]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+
+    clEnqueueSVMMemcpy(
+        queue,
+        CL_TRUE,
+        buffer,
+        TEMP,
+        len,
+        0,
+        nullptr,
+        nullptr
+    );
+}
+
+cl_device_id choose_device() {
+    uint32_t platform_count = 0;
+    
+    clGetPlatformIDs(0, nullptr, &platform_count);
+    cl_platform_id platforms[platform_count];
+    if (platform_count == 0) {
+        printf("No OpenCL platforms found! Simulation aborted!\n");
+        exit(0);
+    }
+    clGetPlatformIDs(platform_count, platforms, nullptr);
+
+    uint32_t supported_platform_count = 0;
+    cl_platform_id supported_platforms[platform_count];
+
+    uint32_t supported_device_count = 0;
+    for(uint32_t i = 0; i < platform_count; i++) {
+        size_t version_data_len;
+        clGetPlatformInfo(
+            platforms[i],
+            CL_PLATFORM_VERSION,
+            0,
+            nullptr,
+            &version_data_len
+        );
+        char version_data[version_data_len];
+        clGetPlatformInfo(
+            platforms[i],
+            CL_PLATFORM_VERSION,
+            version_data_len,
+            version_data,
+            nullptr
+        );
+
+        if ((uint8_t) (version_data[7] - '0') >= 2) {
+            supported_platforms[supported_platform_count] = platforms[i];
+
+            uint32_t device_count = 0;
+            clGetDeviceIDs(
+                supported_platforms[supported_platform_count],
+                CL_DEVICE_TYPE_ALL,
+                0,
+                nullptr,
+                &device_count
+            );
+            supported_device_count += device_count;
+
+            supported_platform_count++;
+        }
+    }
+    if (supported_device_count == 0) {
+        printf("No supported OpenCL devices found! Simulation aborted!\n");
+        exit(0);
+    }
+    
+    cl_device_id supported_devices[supported_device_count];
+    uint32_t idx = 0;
+    for (uint32_t i = 0; i < supported_platform_count; i++) {
+        uint32_t device_count = 0;
+        clGetDeviceIDs(
+            supported_platforms[i],
+            CL_DEVICE_TYPE_ALL,
+            0,
+            nullptr,
+            &device_count
+        );
+        clGetDeviceIDs(
+            supported_platforms[i],
+            CL_DEVICE_TYPE_ALL,
+            device_count,
+            &supported_devices[idx],
+            nullptr
+        );
+
+        idx += device_count;
+    }
+    
+    printf("Supported devices available:\n");
+    for(uint32_t i = 0; i < supported_device_count; i++) {
+        size_t name_len;
+        clGetDeviceInfo(
+            supported_devices[i],
+            CL_DEVICE_NAME,
+            0,
+            nullptr,
+            &name_len
+        );
+        char name[name_len];
+        clGetDeviceInfo(
+            supported_devices[i],
+            CL_DEVICE_NAME,
+            name_len,
+            name,
+            nullptr
+        );
+        printf("%u] %s\n", i + 1, name);
+    }
+    printf("\n");
+
+    printf("Choose device (number): ");
+    uint32_t idx2 = 0;
+    scanf("%u", &idx2);
+    if (idx2 < 1) {
+        printf("\nInvalid selection! Input should be 1 or greater than that!\n");
+        exit(1);
+    }
+    else if (idx2 > supported_device_count) {
+        printf("\nInvalid selection! Device number doesnt exist!\n");
+        exit(1);
+    }
+    
+    return supported_devices[idx2 - 1];
+}
